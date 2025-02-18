c0bdb81a4a9bc19393b6b29d09ff26ffa2a2bf42
SachinMishra-ux <sachin19566@gmail.com>
2025-02-18 19:25:25 +0530

    added reqiurements.txt

--- experiments/authenticate.py
+++ experiments/authenticate.py
@@ -24,7 +24,7 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '/Users/sachinmishra/Desktop/personal/Youtubedata/credentials.json', SCOPES)
+                '/Users/sachinmishra/Desktop/Youtube_subscriber_service/experiments/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
@@ -32,8 +32,7 @@ def authenticate():
 
     return creds
 
-'''
+
 if __name__ == '__main__':
     credentials = authenticate()
     print("Authentication Successful!")
-'''
